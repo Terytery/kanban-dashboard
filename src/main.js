1887f5e04a2cb1249224e2fc4802e09b88d85258
Terytery <64370344+Terytery@users.noreply.github.com>
2020-11-26 12:52:57 +0100

    Add store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import { store } from './store/store';
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
