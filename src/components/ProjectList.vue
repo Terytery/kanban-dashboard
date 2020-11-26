402871786d32d5d9192999d71ca264e1f8970414
RemiBarthe <barthe.rem@gmail.com>
2020-11-26 22:28:24 +0100

    Project init and new components

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>SALUT</v-container>
+</template>
+
+<script>
+export default {
+  name: "ProjectList",
+
+  data: () => ({})
+};
+</script>
---
+++ src/containers/ProjectContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <ProjectList />
+</template>
+
+<script>
+import ProjectList from "../components/ProjectList";
+
+export default {
+  name: "ProjectContainer",
+  components: {
+    ProjectList
+  },
+  data: () => ({})
+};
+</script>
