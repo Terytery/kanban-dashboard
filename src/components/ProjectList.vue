5b2f5f8e589035254d165cc7dc3e887a9ab70209
rgl243 <64373934+rgl243@users.noreply.github.com>
2020-11-27 01:02:09 +0100

    create vue projectlist

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,11 +1,71 @@
 <template>
-  <v-container>SALUT</v-container>
+  <v-container>
+    <v-row align="right" justify="space-between">
+      <t1>Liste des projets</t1>
+      <v-btn rounded color="success"> New </v-btn>
+    </v-row>
+    <br />
+    <v-simple-table>
+      <template v-slot:default>
+        <tbody>
+          <tr>
+            <td>Project 1</td>
+            <td>
+              <v-row align="center" justify="end">
+                <p class="margin">26-11-2020</p>
+                <v-btn small rounded color="primary" class="margin">
+                  Commit
+                </v-btn>
+                <v-btn small rounded color="error" class="margin">
+                  Delete
+                </v-btn>
+              </v-row>
+            </td>
+          </tr>
+          <tr>
+            <td>Project 2</td>
+            <td>
+              <v-row align="center" justify="end">
+                <p class="margin">27-11-2020</p>
+                <v-btn small rounded color="primary" class="margin">
+                  Commit
+                </v-btn>
+                <v-btn small rounded color="error" class="margin">
+                  Delete
+                </v-btn>
+              </v-row>
+            </td>
+          </tr>
+          <tr>
+            <td>Project 3</td>
+            <td>
+              <v-row align="center" justify="end">
+                <p class="margin">28-11-2020</p>
+                <v-btn small rounded color="primary" class="margin">
+                  Commit
+                </v-btn>
+                <v-btn small rounded color="error" class="margin">
+                  Delete
+                </v-btn>
+              </v-row>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "ProjectList",
 
-  data: () => ({})
+  data: () => ({}),
 };
 </script>
+
+<style scoped>
+.margin {
+  margin: 0px 15px;
+}
+</style>
