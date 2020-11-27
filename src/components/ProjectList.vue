d6b64645e715c6c7f5e342347b16253f6b2b640e
Terytery <64370344+Terytery@users.noreply.github.com>
2020-11-27 12:32:40 +0100

    Data structure (#2)
    * Update structure store
    
    * BDD link

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,11 +1,34 @@
 <template>
-  <v-container>SALUT</v-container>
+  <v-container>
+    <v-card v-for="project in projects" :key="project.id">
+      {{ project.name }}
+    </v-card>
+    <v-btn @click="createProject"> Ajouter </v-btn>
+  </v-container>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import { db } from "../firebase";
+
 export default {
   name: "ProjectList",
+  data: () => ({}),
+  mounted() {
+    this.$store.dispatch("getProjects");
+  },
+  computed: {
+    ...mapState(["projects"]),
+  },
 
-  data: () => ({})
+  methods: {
+    createProject() {
+      const project = {
+        name: "tot",
+        dateStart: "27-11-2020",
+      };
+      db.collection("projects").add(project);
+    },
+  },
 };
 </script>
