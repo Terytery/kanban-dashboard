26a9b09e025f0714db3dd46d2b4e57a967568d71
rgl243 <64373934+rgl243@users.noreply.github.com>
2020-11-29 23:52:25 +0100

    Add edit-project

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -18,13 +18,61 @@
 
       <template v-slot:[`item.actions`]="{ item }">
         <td @click.stop>
-          <v-icon small class="mr-2" @click="editProject(item)">
-            mdi-pencil
-          </v-icon>
+          <v-icon small class="mr-2" @click="confirmEdit(item)">
+            mdi-pencil</v-icon
+          >
           <v-icon small @click="confirmDelete(item)"> mdi-delete</v-icon>
         </td>
       </template>
     </v-data-table>
+
+    <v-dialog v-model="modalEditProject" width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Modifier le projet</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="projectToEdit.name"
+                  label="Nom du projet*"
+                  type="text"
+                  required
+                  hide-details="auto"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="projectToEdit.dateStart"
+                  label="Date de début*"
+                  type="date"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="projectToEdit.dateEnd"
+                  label="Date de fin"
+                  type="date"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Champs obligatoire</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" text @click="modalEditProject = false">
+            Fermer
+          </v-btn>
+          <v-btn color="blue" text @click="editProject(projectToEdit)">
+            Modifier
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
     <v-dialog v-model="modalDeleteConfirm" width="750">
       <v-card>
@@ -82,6 +130,8 @@ export default {
       { text: "Actions", value: "actions", sortable: false }
     ],
     modalDeleteConfirm: false,
+    modalEditProject: false,
+    projectToEdit: {},
     projectToDelete: {}
   }),
   mounted() {
@@ -96,8 +146,14 @@ export default {
     openProject(project) {
       this.$store.dispatch("setCurrentProject", project);
     },
+    confirmEdit(item) {
+      this.projectToEdit = Object.assign({}, item);
+      this.modalEditProject = true;
+    },
     editProject(item) {
-      console.log(item);
+      this.modalEditProject = false;
+      this.projectToEdit = {};
+      db.collection("projects").doc(item.id).update(item);
     },
     confirmDelete(item) {
       this.projectToDelete = item;
@@ -105,9 +161,7 @@ export default {
     },
     deleteProject(item) {
       this.modalDeleteConfirm = false;
-      db.collection("projects")
-        .doc(item)
-        .delete();
+      db.collection("projects").doc(item).delete();
     }
   }
 };
