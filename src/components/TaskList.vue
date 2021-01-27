7977cb6ff4da5a58232228ea8789d033b42c01b5
RemiBarthe <barthe.rem@gmail.com>
2021-01-27 23:29:13 +0100

    Disable draggable for guest

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -2,7 +2,21 @@
   <v-col>
     <v-list class="task-list" outlined>
       <v-subheader>{{ title }}</v-subheader>
-      <draggable group="tasks" v-model="orderedTasks" class="draggable-test">
+      <template v-if="isGuest">
+        <TaskItem
+          v-for="task in orderedTasks"
+          v-bind:key="task.id"
+          :task="task"
+          @changeUser="changeUser"
+        />
+      </template>
+
+      <draggable
+        v-else
+        group="tasks"
+        v-model="orderedTasks"
+        class="draggable-test"
+      >
         <TaskItem
           v-for="task in orderedTasks"
           v-bind:key="task.id"
@@ -37,7 +51,7 @@ export default {
     category: String
   },
   computed: {
-    ...mapState(["currentProject"]),
+    ...mapState(["currentProject", "connectedUser"]),
     orderedTasks: {
       get() {
         return _.orderBy(this.tasks, "position");
@@ -56,6 +70,9 @@ export default {
           i++;
         });
       }
+    },
+    isGuest() {
+      return this.connectedUser.tokenId === "guest";
     }
   },
   methods: {
